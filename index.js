4ba2a76e37bab338c2c8eaee6e1a8833089cfd08
karlaestrada861 <113952707+karlaestrada01@users.noreply.github.com>
2022-10-18 22:59:40 -0400

    Adding clipboard feature

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const colorTwo = document.getElementById("color-two")
 const colorThree = document.getElementById("color-three")
 const colorFour = document.getElementById("color-four")
 const colorFive = document.getElementById("color-five")
-
+const colorContainer = document.querySelector(".color-container")
 let color
 let mode
 
@@ -38,7 +38,20 @@ document.getElementById("color-form").addEventListener("submit", (e) => {
         colorFive.nextElementSibling.textContent = data.colors[3].hex.value
     }) 
 
-    console.log(color)
+    colorContainer.addEventListener("click", (event) => {
+        let currentEvent = event.target.id
+        if(currentEvent === "color-one-p" || (currentEvent === "color-two-p") ||
+        (currentEvent === "color-three-p") || (currentEvent === "color-four-p") || currentEvent === "color-five-p"){
+            navigator.clipboard.writeText(document.getElementById(currentEvent).textContent)
+            console.log(document.getElementById(event.target.id).textContent)
+        }
+        else {
+            console.log("loser")
+        }
+        
+    })
+
 }) 
 
 
+//add default color to #5C1414
