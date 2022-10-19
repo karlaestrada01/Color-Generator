45333d60b87e8baf8207d4daa19363b6ddf0e58d
karlaestrada861 <113952707+karlaestrada01@users.noreply.github.com>
2022-10-18 22:21:24 -0400

    Fetching the color api and styling my color generator

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
 const box = document.querySelector(".box")
 
+const colorOne = document.getElementById("color-one")
+const colorTwo = document.getElementById("color-two")
+const colorThree = document.getElementById("color-three")
+const colorFour = document.getElementById("color-four")
+const colorFive = document.getElementById("color-five")
 
-fetch("https://www.thecolorapi.com/scheme?hex=24B1E0&mode=triad&count=6 ")
+let color
+let mode
+
+document.getElementById("color-form").addEventListener("submit", (e) => {
+    e.preventDefault()
+    color = document.getElementById("color-input").value
+    mode = document.getElementById("type-input").value
+
+    fetch(`https://www.thecolorapi.com/scheme?hex=${color.slice(1)}&mode=${mode}&count=5`)
     .then(response => response.json())
     .then(data => {
-        console.log(data)
+        
+       
+       
+        colorOne.style.backgroundColor = color
+        colorOne.nextElementSibling.textContent = color
+
+        
+        colorTwo.style.backgroundColor = data.colors[0].hex.value
+        colorTwo.nextElementSibling.textContent = data.colors[0].hex.value
+
+        colorThree.style.backgroundColor = data.colors[1].hex.value
+        colorThree.nextElementSibling.textContent =  data.colors[1].hex.value
+
+       
+        colorFour.style.backgroundColor = data.colors[2].hex.value
+        colorFour.nextElementSibling.textContent = data.colors[2].hex.value
+
+        colorFive.style.backgroundColor = data.colors[3].hex.value
+        colorFive.nextElementSibling.textContent = data.colors[3].hex.value
     }) 
+
+    console.log(color)
+}) 
+
+
