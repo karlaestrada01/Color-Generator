d9ff7f62a3356b482d9fd354d240746e3039e516
karlaestrada861 <113952707+karlaestrada01@users.noreply.github.com>
2022-10-19 20:46:42 -0400

    Adding comments and cleaning up the code

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-
+/**
+ * Author: Karla Estrada
+ * Version: 1.0
+ * Date: 10-19-2022
+ * 
+ * Color Generator 
+ *      Generates a palette of color based on the user's input of color and mode using the color api
+ */
 const colorOne = document.getElementById("color-one")
 const colorTwo = document.getElementById("color-two")
 const colorThree = document.getElementById("color-three")
@@ -8,6 +15,9 @@ const colorContainer = document.querySelector(".color-container")
 let color
 let mode
 
+/**
+ * Event listener for when the form is submitted
+ */
 document.getElementById("color-form").addEventListener("submit", (e) => {
     e.preventDefault()
     color = document.getElementById("color-input").value
@@ -16,9 +26,6 @@ document.getElementById("color-form").addEventListener("submit", (e) => {
     fetch(`https://www.thecolorapi.com/scheme?hex=${color.slice(1)}&mode=${mode}&count=5`)
     .then(response => response.json())
     .then(data => {
-        
-       
-       
         colorOne.style.backgroundColor = color
         colorOne.nextElementSibling.textContent = color
 
@@ -37,6 +44,9 @@ document.getElementById("color-form").addEventListener("submit", (e) => {
         colorFive.nextElementSibling.textContent = data.colors[3].hex.value
     }) 
 
+    /**
+     * Event listener when a user click on the hex value of a color
+     */
     colorContainer.addEventListener("click", (event) => {
         let currentEvent = event.target.id
         if(currentEvent === "color-one-p" || (currentEvent === "color-two-p") ||
